5e43aa7c54ed5003b56fc5dacdad0b74afc00a12
Jeff Ellis <jeff@ellises.us>
2020-07-25 17:42:16 -0400

    - Basic scorecard functionality

--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,16 +1,82 @@
-import React from 'react';
+import React from "react";
 
-import './Row.scss';
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import ToggleButton from "react-bootstrap/ToggleButton";
 
-const displayName = 'Row';
+import "./Row.scss";
+import { STATES, LOCK_COLUMN } from "../Constants";
+
+const displayName = "Row";
 
 const Row = (props) => {
+  const onBoxToggled = (event) => {
+    props.onBoxToggled(props.color, event.target.value);
+  };
 
-    return (
-        <div className={`${displayName} ${displayName}-${props.color}`}>
+  const getButtons = () => {
+    const buttonProps = {
+      onChange: onBoxToggled,
+      size: "lg",
+      type: "checkbox",
+      variant: "outline-primary",
+    };
 
-        </div>
-    );
+    const buttons = [];
+    for (let i = 2; i <= 12; i++) {
+      const extraProps = {
+        className: `btn-${props.boxState[i]} btn-${i}`,
+        key: i,
+        value: i,
+      };
+
+      switch (props.boxState[i]) {
+        case STATES.BLOCKED:
+        case STATES.LOCKED:
+          extraProps.disabled = true;
+          break;
+
+        case STATES.WON:
+          extraProps.checked = true;
+          break;
+
+        default:
+          break;
+      }
+
+      buttons.push(
+        <ToggleButton {...buttonProps} {...extraProps}>
+          {i}
+        </ToggleButton>
+      );
+    }
+
+    if (props.reversed) {
+      buttons.reverse();
+    }
+
+    const lockButtonProps = {
+      ...buttonProps,
+      className: `btn-${props.boxState[LOCK_COLUMN]} btn-lock`,
+      key: LOCK_COLUMN,
+      value: LOCK_COLUMN,
+    };
+
+    buttons.push(<ToggleButton {...lockButtonProps}>&nbsp;</ToggleButton>);
+
+    return buttons;
+  };
+
+  const toggleButtonGroupProps = {
+    type: "checkbox",
+  };
+
+  return (
+    <div className={`${displayName} ${displayName}-${props.color}`}>
+      <ToggleButtonGroup {...toggleButtonGroupProps}>
+        {getButtons(props)}
+      </ToggleButtonGroup>
+    </div>
+  );
 };
 
 export default Row;
