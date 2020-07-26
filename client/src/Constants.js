6719e947c584cb01595cdb98467d6b7f897df932
Jeff Ellis <jeff@ellises.us>
2020-07-26 16:07:25 -0400

    - Add points list

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,8 +1,12 @@
+export const LOCK_COLUMN = 13;
+
+export const PENALTY = -5;
+
+export const POINT_VALUES = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78];
+
 export const STATES = {
   BLOCKED: "BLOCKED",
   OPEN: "OPEN",
   WON: "WON",
   LOCKED: "LOCKED",
 };
-
-export const LOCK_COLUMN = 13;
