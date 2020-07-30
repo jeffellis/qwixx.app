f2b0b10ab5ca813a2a5b113a2cbfaba582dc6fbe
Jeff Ellis <jeff@ellises.us>
2020-07-30 15:42:55 -0400

    - store the card state in idb to persist across reloads, etc.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import App from "./App";
+import { initDB, getScoreCard, saveScoreCard } from "./services/DBService";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as serviceWorker from "./serviceWorker";
+
+async function init() {
+  await initDB();
+  const card = await getScoreCard();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+init();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
