b9e0307521f150cc9cb0c8f0072a73f21e37239e
Jeff Ellis <jeff@ellises.us>
2020-08-12 17:47:25 -0400

    Updates

---
+++ precache-manifest.de0d609fed499fe441c21268637945c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324acd7db083b97248459de5c0144e08",
+    "url": "/qwixx.app/index.html"
+  },
+  {
+    "revision": "0348c21059c51909bc58",
+    "url": "/qwixx.app/static/css/2.e5b300ba.chunk.css"
+  },
+  {
+    "revision": "4d8247e3c8b91bc36625",
+    "url": "/qwixx.app/static/css/main.14edf133.chunk.css"
+  },
+  {
+    "revision": "0348c21059c51909bc58",
+    "url": "/qwixx.app/static/js/2.63da58e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/qwixx.app/static/js/2.63da58e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8247e3c8b91bc36625",
+    "url": "/qwixx.app/static/js/main.97428a55.chunk.js"
+  },
+  {
+    "revision": "dbebaddbd85a0eeb4ea6",
+    "url": "/qwixx.app/static/js/runtime-main.47e59be7.js"
+  }
+]);
