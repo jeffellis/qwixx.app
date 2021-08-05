113610cc4fd13dbbeb145b062b2007fd724a6cf2
Jeff Ellis <jeff@ellises.us>
2021-08-05 15:42:59 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 import ScoreCard from "./components/ScoreCard";
+import firebase from "./firebase/firebase";
+import FirebaseContext from "./firebase/FirebaseContext";
+
 import "./App.scss";
 
 function App() {
+
+  const firebaseContext = {
+    firebase,
+  };
+
   return (
-    <div className="App">
-      <ScoreCard />
-    </div>
+    <FirebaseContext.Provider value={firebaseContext}>
+      <div className="App">
+        <ScoreCard />
+      </div>
+    </FirebaseContext.Provider>
   );
 }
 
---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,18 @@
+ // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0sc9my2OkF_vkEb1DmehRA97mWBHOsgI",
+    authDomain: "qwixx-app.firebaseapp.com",
+    databaseURL: "https://qwixx-app-default-rtdb.firebaseio.com",
+    projectId: "qwixx-app",
+    storageBucket: "qwixx-app.appspot.com",
+    messagingSenderId: "270987756365",
+    appId: "1:270987756365:web:a95b416001c0bfcbf1ed04",
+    measurementId: "G-LMGL9YSP0W"
+};
+
+// Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+export default firebaseConfig;
