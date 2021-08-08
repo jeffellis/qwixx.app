81d3b4cb9f73d893346ff6f7500f19800c732616
Jeff Ellis <jeff@ellises.us>
2021-08-08 17:35:55 -0400

    Updates

---
+++ precache-manifest.3d2f90b5fae27b2df272690ad48c4fc9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0d48c2fc7afefe023ea3fd521c417b",
+    "url": "/qwixx.app/index.html"
+  },
+  {
+    "revision": "720989c2de18dd6e9448",
+    "url": "/qwixx.app/static/css/2.550a6f95.chunk.css"
+  },
+  {
+    "revision": "2902f8b4ffe649eed85f",
+    "url": "/qwixx.app/static/css/main.8f7db3ea.chunk.css"
+  },
+  {
+    "revision": "720989c2de18dd6e9448",
+    "url": "/qwixx.app/static/js/2.0b9b13fa.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/qwixx.app/static/js/2.0b9b13fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2902f8b4ffe649eed85f",
+    "url": "/qwixx.app/static/js/main.c7ba67c7.chunk.js"
+  },
+  {
+    "revision": "dbebaddbd85a0eeb4ea6",
+    "url": "/qwixx.app/static/js/runtime-main.47e59be7.js"
+  },
+  {
+    "revision": "d7c37d6ec4898294094ba872d71a9208",
+    "url": "/qwixx.app/static/media/ic_block_48px.d7c37d6e.svg"
+  },
+  {
+    "revision": "9cf15f6c1c04d6d7751b6c0c326ac6c1",
+    "url": "/qwixx.app/static/media/ic_done_48px.9cf15f6c.svg"
+  },
+  {
+    "revision": "37490c0a4ab11d38acfc9110e6f245fa",
+    "url": "/qwixx.app/static/media/ic_lock_48px.37490c0a.svg"
+  },
+  {
+    "revision": "4e31b18750541b80cd9e7782728d0f7d",
+    "url": "/qwixx.app/static/media/ic_lock_open_48px.4e31b187.svg"
+  },
+  {
+    "revision": "677b06dbbb458fbf8a40de2424043661",
+    "url": "/qwixx.app/static/media/ic_power_settings_new_24px.677b06db.svg"
+  }
+]);
